a0718643ace22644d9b3e02353ea88da7c0f01c0
tow5532 <tow5532@gmail.com>
2021-02-18 14:16:03 +0900

    all files

---
+++ app/Http/Controllers/SlotApiController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class SlotApiController extends Controller
+{
+    public function index()
+    {
+
+        $endpoint = "http://slot99n.com/createplayer.html";
+        $client = new Client();
+
+        $response = $client->request('GET', $endpoint, ['query' => [
+            'adminid' => 'Steve',
+            'playerid' => 'test1',
+            'playername' => '테스트1계정',
+            'bankcd' => '테스트은행',
+            'banknum' => '111-1111-1111',
+            'bankname' => '예금주',
+        ]]);
+
+        $statusCode = $response->getStatusCode();
+        $content = $response->getBody();
+
+        dd($content);
+    }
+}
